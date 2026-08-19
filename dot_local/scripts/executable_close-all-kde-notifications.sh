a6e9b478ff94386f8894c644abc2658a28209835
Joe "XezolesS" K. <tndid7876@gmail.com>
2026-08-20 00:58:53 +0900

    feat: update script

--- dot_local/scripts/executable_close-all-kde-notifications.sh
+++ dot_local/scripts/executable_close-all-kde-notifications.sh
@@ -23,10 +23,10 @@ while true; do
   esac
 done
 
-for i in $(seq $(notify-send -p " " -t 1) -1 0); do
-  qdbus org.kde.plasmashell \
+for i in $(seq "$(notify-send -p " " -t 1)" -1 0); do
+  qdbus6 org.kde.plasmashell \
     /org/freedesktop/Notifications \
-    org.freedesktop.Notifications.CloseNotification $i
+    org.freedesktop.Notifications.CloseNotification "$i"
 done
 
 if [ $SILENT -ne 1 ]; then
